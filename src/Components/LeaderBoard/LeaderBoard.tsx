77acde1781411f992bc4eec932612bcb4fa4150e
akabhowmick <47453997+akabhowmick@users.noreply.github.com>
2023-11-17 23:50:41 -0500

    fixed leaderboard screen

--- src/Components/LeaderBoard/LeaderBoard.tsx
+++ src/Components/LeaderBoard/LeaderBoard.tsx
@@ -1,3 +1,4 @@
+import { useRetroAppContext } from "../../providers/RetroAppProvider";
 import { Player } from "../../types/interfaces";
 import "./LeaderBoard.css";
 import { PlayerEntry } from "./PlayerEntry";
@@ -15,15 +16,37 @@ const playerArray: Player[] = [
     name: "Player 3",
     score: 600,
   },
+  {
+    name: "Player 4",
+    score: 500,
+  },
+  {
+    name: "Player 5",
+    score: 400,
+  },
 ];
 
 export const LeaderBoard = () => {
+  const { setCurrentScreen } = useRetroAppContext();
+
   return (
-    <div className="leaderboard">
-      <div className="header">Retro Leaderboard</div>
-      {playerArray.map((player, index) => {
-        return <PlayerEntry key={index} position={index} player={player} />;
-      })}
+    <div className="start-page-wrapper">
+      <div className="lb-btn-container">
+        <button
+          className="lb-btn"
+          onClick={() => {
+            setCurrentScreen("");
+          }}
+        >
+          Back to Start!
+        </button>
+      </div>
+      <div className="leaderboard">
+          <div className="header">Retro Leaderboard</div>
+          {playerArray.map((player, index) => {
+            return <PlayerEntry key={index} position={index} player={player} />;
+          })}
+        </div>
     </div>
   );
 };
